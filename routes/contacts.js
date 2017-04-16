6780bfe2bdb7f9310965cd550dd48ff607d78146
Cheryln <cherylnbarber@gmail.com>
2017-04-15 18:05:23 -0700

    Posting works but does not validate if the address already exists and it doesn't yet respond to the drop down selection.

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,6 +12,41 @@ router.get('/', (req, res)=>{
   });
 });
 
+router.post('/', function(req, res){
+  var contact = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    phone_number: req.body.phone_number,
+    email_address: req.body.email,
+    image_url: req.body.image_url,
+  };
+  var inputAddress = {
+    line_1: req.body.line_1,
+    line_2: req.body.line_2,
+    city: req.body.city,
+    zip: req.body.zip
+  };
+
+  knex('addresses').insert(inputAddress, '*').then((newAddress)=>{
+    let id = newAddress[0].id
+    contact.addresses_id = id;
+    console.log(contact);
+    knex('contacts').insert(contact, '*').then(function(newContact){
+      let id = newContact[0].id;
+      res.redirect(`/contacts/${id}`)
+    });
+
+  });
+});
+
+router.get('/create', (req, res)=>{
+  knex('addresses').innerJoin('contacts', 'addresses.id', 'contacts.addresses_id').then((contact)=>{
+    res.render('contacts/create', {
+      contact
+    })
+  })
+})
+
 router.delete('/:id', (req, res)=>{
   var obj = {}
   var id = req.params.id;
@@ -23,24 +58,23 @@ router.delete('/:id', (req, res)=>{
     knex('addresses').del().where('addresses.id', obj.addressid).then(()=>{
       knex('contacts').del().where('contacts.id', id).then(()=>{
         res.redirect('/contacts');
-      })
-    })
-
+      });
+    });
   });
-  // return obj
 });
 
 
-//get one
-// router.get('/:id', (req, res)=>{
-//   var id = req.params.id;
-//   knex('contacts').innerJoin('addresses', 'addresses.id', 'contacts.addresses_id').where('contacts.id', id).first()
-//   .then((contact)=>{
-//     res.render('contacts/contact', {
-//       contact
-//     });
-//   });
-// });
+
+
+// get one
+router.get('/:id', (req, res)=>{
+  var id = req.params.id;
+  knex('contacts').innerJoin('addresses', 'addresses.id', 'contacts.addresses_id').where('contacts.id', id).first().then((contact)=>{
+    res.render('contacts/contact', {
+      contact
+    });
+  });
+});
 
 
 module.exports = router;
